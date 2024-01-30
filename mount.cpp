de554839496a76f601d02420c2d75c3fb034f805
Danila <danil.akulov.95@mail.ru>
2024-01-30 14:12:32 +0500

    transfer files

--- mount.cpp
+++ mount.cpp
@@ -1,5 +1,6 @@
 ﻿#include <cstdlib> 
 #include <iostream>
+#include <fstream>
 #include <string>
 
 
@@ -18,31 +19,79 @@ bool DriveMount(string device, string mount_point) {
 bool DriveUnmount(string mount_point) {
     string command = "sudo umount " + mount_point;
     if (system(command.c_str()) == -1) {
-        cout << mount_point << " не удалось отмонтировать ." << endl;
+        cout << mount_point << " не удалось отмонтировать." << endl;
         return false;
     }
     cout << mount_point << " отмонтирован." << endl;
     return true;
 }
 
+bool FileTransfer(string filePath, string mount_dir) {
+    std::ifstream source;
+    std::ofstream destination;
+
+    source.open(filePath, std::ios::binary);
+    if (!source) {
+        std::cout << "Не удалось открыть исходный файл '" << filePath << "'." << std::endl;
+        return false;
+    }
+    
+    std::string path = mount_dir + filePath;
+    if (!path.empty() && path.back() == '/') {
+        path.pop_back();
+    }
+    destination.open(path, std::ios::binary);
+    if (!destination) {
+        std::cout << "Не удалос открыть файл назначения '" << path << "'." << std::endl;
+        return false;
+    }
+        
+    destination << source.rdbuf();
+    source.close();
+    destination.close();
+    return true;
+}
+
 int main() {
     string device;
     string mount_dir;
+    string pathToFile;
+    bool working = true;
 
     system("lsblk");
-    printf("Введите имя носителя:");
-    std::cin >> device;
+    printf("Введите имя носителя: ");
+    cin >> device;
+    cin.ignore();
     device = "/dev/" + device;
 
-    cout << "Директория для монтирования (0 - по умолчанию '/media/flash'): ";
-    cin >> mount_dir;
+    cout << "Директория для монтирования (0 - по умолчанию '/media/flash/'): ";
+    getline(cin, mount_dir);
+    cin.ignore();
     if (mount_dir == "0") {
-        mount_dir = "/media/flash";
-    };
+        mount_dir = "/media/flash/";
+    }
+    string mkdir = "mkdir -p " + mount_dir;
+    system(mkdir.c_str());
 
     if (!DriveMount(device, mount_dir)) {
-        cerr << "Ошибка монтирования диска." << endl;
+        cout << "Ошибка монтирования диска." << endl;
         exit(EXIT_FAILURE);
+    }
+
+    while (working){
+        cout << "Путь до файла, который необходимо скопировать на носитель (end - завершение): ";
+        getline(cin, pathToFile);
+        cin.ignore();
+        if (pathToFile == "end") {
+            working = false;
+            break;
+        }
+        if (FileTransfer(pathToFile, mount_dir)) {
+            std::cout << "Копирование выполнено!" << std::endl;
+        }
+        else {
+            std::cout << "Ошибка при копировании файла." << std::endl;
+        }
     }
 
     if (!DriveUnmount(mount_dir)) {
